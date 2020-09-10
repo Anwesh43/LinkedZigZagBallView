ff6944b34006df2dff1855ee7bcbbed2c3fd283a
Anwesh43 <anweshthecool0@gmail.com>
2020-09-11 02:42:02 +0530

    adding Canvas extension methods

--- zigzagballview/src/main/java/com/anwesh/uiprojects/zigzagballview/ZigZagBallView.kt
+++ zigzagballview/src/main/java/com/anwesh/uiprojects/zigzagballview/ZigZagBallView.kt
@@ -22,7 +22,7 @@ val colors : Array<Int> = arrayOf(
 val divs : Int = 3
 val parts : Int = 2 + divs
 val scGap : Float = 0.02f / parts
-val rFactor : Float = 8.9f
+val rFactor : Float = 13.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 
@@ -30,3 +30,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.max(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawZigZagBall(scale : Float, w : Float, h : Float, paint : Paint) {
+    val scFirst : Float = scale.divideScale(0, parts)
+    val scLast : Float = scale.divideScale(parts - 1, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    val gap : Float = w
+    save()
+    translate(0f, h / 2)
+    for (i in 0..1) {
+        var x : Float = 0f
+        var y : Float = 0f
+        save()
+        scale(1f, 1f - 2 * i)
+        for (j in 1..divs) {
+            val scj : Float = scale.divideScale(j, parts)
+            x += gap * scj
+            y -= gap * scj.sinify()
+        }
+        drawCircle(x, y, r * (scFirst - scLast), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawZZBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawZigZagBall(scale, w, h, paint)
+}
