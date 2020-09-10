5cf8d434d5321e6eb899825a1e53fa4616b3e9ab
Anwesh43 <anweshthecool0@gmail.com>
2020-09-11 02:34:58 +0530

    adding Int and Float extension methods

--- zigzagballview/src/main/java/com/anwesh/uiprojects/zigzagballview/ZigZagBallView.kt
+++ zigzagballview/src/main/java/com/anwesh/uiprojects/zigzagballview/ZigZagBallView.kt
@@ -25,3 +25,8 @@ val scGap : Float = 0.02f / parts
 val rFactor : Float = 8.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.max(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
