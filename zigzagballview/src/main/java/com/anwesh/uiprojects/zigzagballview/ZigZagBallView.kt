df834eb998834f848fb5951a0ab610b2927385e1
Anwesh43 <anweshthecool0@gmail.com>
2020-09-11 02:43:25 +0530

    adding ZigZagBallView class

--- zigzagballview/src/main/java/com/anwesh/uiprojects/zigzagballview/ZigZagBallView.kt
+++ zigzagballview/src/main/java/com/anwesh/uiprojects/zigzagballview/ZigZagBallView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawZZBNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawZigZagBall(scale, w, h, paint)
 }
+
+class ZigZagBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
